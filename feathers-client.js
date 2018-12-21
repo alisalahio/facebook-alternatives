a3b35e1ac241fdf4cf424eeb0cd0c660ec303331
Ali Salah <ali.salah.sheikh@gmail.com>
2018-12-21 11:35:56 +0200

    fix

--- feathers-client.js
+++ feathers-client.js
@@ -5,8 +5,9 @@ import io from 'socket.io-client'
 export const host = process.env.VUE_APP_SERVER_URL
 
 const socket = io(host, {
-  transports: ['websocket']
-})
+  transports: ["websocket"],
+  forceNew: true
+});
 
 const feathersClient = feathers()
   .configure(socketio(socket))
